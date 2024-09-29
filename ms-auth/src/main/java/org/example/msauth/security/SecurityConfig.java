a350896dbf29e82d42963d32c4e41e4819edc641
AlexCoilaJrt <104539425+AlexCoilaJrt@users.noreply.github.com>
2024-09-29 12:29:31 -0500

    asdasd

--- ms-auth/src/main/java/org/example/msauth/security/SecurityConfig.java
+++ ms-auth/src/main/java/org/example/msauth/security/SecurityConfig.java
@@ -9,13 +9,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable() // Desactiva la protección CSRF (para APIs REST puede ser necesario)
+        http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth/login", "/auth/register").permitAll() // Permitir acceso público a login y registro
-                .antMatchers("/admin/**").hasRole("ADMIN") // Solo accesible para ADMIN
-                .antMatchers("/dictaminador/**").hasRole("DICTAMINADOR") // Solo accesible para DICTAMINADOR
-                .anyRequest().authenticated() // Cualquier otra ruta requiere autenticación
+                .antMatchers("/auth/login", "/auth/register").permitAll() // Permitir login y registro sin autenticación
+                .antMatchers("/admin/**").hasRole("ADMIN") // Acceso para rol ADMIN
+                .antMatchers("/dictaminador/**").hasRole("DICTAMINADOR") // Acceso para rol DICTAMINADOR
+                .anyRequest().authenticated() // Otras rutas requieren autenticación
                 .and()
-                .formLogin(); // Utiliza autenticación basada en formularios con sesiones.
+                .formLogin(); // Usar autenticación de formulario
     }
 }
